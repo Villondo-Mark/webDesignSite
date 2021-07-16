299f601b3f58b74b6fcdcc8f89cbed4256aafd90
Villondo-Mark <32647158+Villondo-Mark@users.noreply.github.com>
2021-07-16 14:58:00 -0400

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,17 @@
+function animationFunction() {
+  
+    var animationPlay = document.getElementById("sun"); animationPlay.style.animation="risingSunAnimation 3s ease-in-out 1 forwards 3s";
+    
+      var leafFloat = document.getElementById("leaf1");
+    
+    leafFloat.style.animation="leafanimations ease-in-out forwards 2.4s 6s";
+    
+      var leafFloat2 = document.getElementById("leaf2");
+    
+    leafFloat2.style.animation="leafanimations ease-in-out forwards 2.4s 6.8s";
+    
+        var leafFloat3 = document.getElementById("leaf3");
+    
+    leafFloat3.style.animation="leafanimations ease-in-out forwards 2.4s 6.3s";
+  
+  }
